66d8bf52e96d5afffafad0296df5415969732477
gitrog <37446909+brmetzger@users.noreply.github.com>
2024-10-25 21:25:13 +0200

    Complete Problem 15

--- latticepaths.py
+++ latticepaths.py
@@ -4,21 +4,28 @@ Project Euler Problem 15: Lattice Paths
 
 __author__ = "Benjamin R. Metzger"
 
-def lattice_paths(size_x: int, size_y: int) -> int:
-    if size_x == 0 and size_y == 0:
-        return 0
-    
-    paths: int = 1
-    for x in range(size_x - 1, 0, -1):
-        paths += lattice_paths(x, size_y)
+import math
 
-    for y in range(size_y - 1, 0, -1):
-        paths += lattice_paths(size_x, y)
+#Initial approach, prohibitively slow for larger values
+def lattice_paths_recursive(size_x: int, size_y: int) -> int:
+    if size_x == 0 and size_y == 0:
+        return 1
+    
+    paths: int = 0
+    if size_x > 0:
+        paths += lattice_paths_recursive(size_x - 1, size_y)
+
+    if size_y > 0:
+        paths += lattice_paths_recursive(size_x, size_y - 1)
 
     return paths
 
+def lattice_paths(size: int) -> int:
+    n: int = size * 2
+    return math.factorial(n) // (math.factorial(size) * math.factorial(n - size))
+
 def main():
-    print(lattice_paths(2, 2))
+    print(lattice_paths(20))
     
 
 if __name__ == "__main__":
